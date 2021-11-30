a6bc1b3441590c648b22d70d74cbecaef8a2e0ee
Marianne Azzopardi <marianneazzopardi@Mariannes-MacBook-Air.local>
2021-11-30 12:30:42 +0000

    create user form for sign up and attempting connection with BE

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SignUpForm from './components/UserSignUp'
 
 function App() {
   return (
     <div className="App">
-      
+      <SignUpForm/>
     </div>
   );
 }
