8e71dcf24e447e2687715578f30d9b3e7121ce19
Serena Bertozzi <serenabertozzi@protonmail.com>
2021-12-02 17:26:39 +0000

    more stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 // import Button from '@material-ui/core/Button';
-import './App.css';
-import SignUp from './components/SignUp'
-import { GameView } from './components/Game';
+import "./App.css";
+import SignUp from "./components/SignUp";
+import { GameView } from "./components/Game";
+import LogIn from "./components/LogIn";
 
 const App = () => {
   return (
     <div>
       <div className="App">
         <SignUp />
+      </div>
+      <div className="App">
+        <LogIn />
       </div>
       <div className="Game">
         <GameView />
