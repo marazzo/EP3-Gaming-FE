2bcd0804b0145f49dacd986038c9e093ae918c88
AJOsmaston <dajoxx@gmail.com>
2021-12-02 10:23:39 +0000

    minor formatting, removed unused components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 // import Button from '@material-ui/core/Button';
 import './App.css';
 import SignUp from './components/SignUp'
-
-import { useState } from 'react';
 import { GameView } from './components/Game';
 
 const App = () => {
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,7 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 import { useGameAPI } from '../hooks/useGameAPI';
 
 export const GameView = () => {
-  const [game, changeTurn] = useGameAPI();
-
+  const [game, changeTurn] = useGameAPI(); //[gameData, changeTurn]
   return (
     <div>
       <div>Score: { game.score }</div>
