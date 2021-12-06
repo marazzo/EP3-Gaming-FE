59ac5626cf2e21d30333fbbcce07b0a76c5e0381
Marianne Azzopardi <marianneazzopardi@Mariannes-MacBook-Air.local>
2021-12-06 15:39:34 +0000

    something is happening

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import { Grid } from "@material-ui/core";
+import { Grid, Row, Column } from "@material-ui/core";
 import { useGameAPI } from "../hooks/useGameAPI";
 import { GameOver } from "./GameOver";
 import Hero from "./game_screen/Hero";
@@ -12,17 +12,25 @@ export const GameView = () => {
       changeTurn();
     }
   };
-
   return (
     <div className="game-container">
-      <Grid className="game-bg">
-        <Grid item xs={8}>
-          <Item>
-            <Monster />
-          </Item>
-        </Grid>
+      {/* <Grid >
+        <Monster />
         <Hero />
-      </Grid>
+      </Grid> */}
+      
+  
+      <Grid container className="game-bg">
+    
+        <Grid item xs={6} style={{background: 'red'}}>1</Grid>
+        <Grid item xs = {6} style={{background: 'orange'}}>2</Grid>
+        <Grid item xs = {12} style={{background: 'yellow'}}>3</Grid>
+        <Grid item xs = {4} ><Hero/></Grid>  
+        <Grid item xs = {4} style={{background: 'blue'}}>5</Grid>
+        <Grid item xs = {4} ><Monster/></Grid>
+        <Grid item xs = {12} style={{background: 'grey'}}>7</Grid>
+      </Grid> 
+      {/* test grid */}
       <div>Score: {game.score}</div>
       <div>Health: {game.health}</div>
       <div>
