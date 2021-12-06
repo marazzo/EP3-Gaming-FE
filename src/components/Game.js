947856bbcb3f81936e756578d126d39e045a5a0e
Serena Bertozzi <serenabertozzi@protonmail.com>
2021-12-06 15:55:15 +0000

    Implementa la barra della salute

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,7 @@
-import { Grid, Row, Column } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import { useGameAPI } from "../hooks/useGameAPI";
 import { GameOver } from "./GameOver";
+import HealthBar from "./game_screen/HealthBar";
 import Hero from "./game_screen/Hero";
 import Monster from "./game_screen/Monster";
 
@@ -14,23 +15,30 @@ export const GameView = () => {
   };
   return (
     <div className="game-container">
-      {/* <Grid >
-        <Monster />
-        <Hero />
-      </Grid> */}
-      
-  
       <Grid container className="game-bg">
-    
-        <Grid item xs={6} style={{background: 'red'}}>1</Grid>
-        <Grid item xs = {6} style={{background: 'orange'}}>2</Grid>
-        <Grid item xs = {12} style={{background: 'yellow'}}>3</Grid>
-        <Grid item xs = {4} ><Hero/></Grid>  
-        <Grid item xs = {4} style={{background: 'blue'}}>5</Grid>
-        <Grid item xs = {4} ><Monster/></Grid>
-        <Grid item xs = {12} style={{background: 'grey'}}>7</Grid>
-      </Grid> 
-      {/* test grid */}
+        <Grid item xs={6} style={{ background: "red" }}>
+          <HealthBar game={game} />
+        </Grid>
+        <Grid item xs={6} style={{ background: "orange" }}>
+          2
+        </Grid>
+        <Grid item xs={12} style={{ background: "yellow" }}>
+          3
+        </Grid>
+        <Grid item xs={4}>
+          <Hero />
+        </Grid>
+        <Grid item xs={4} style={{ background: "blue" }}>
+          5
+        </Grid>
+        <Grid item xs={4}>
+          <Monster />
+        </Grid>
+        <Grid item xs={12} style={{ background: "grey" }}>
+          7
+        </Grid>
+      </Grid>
+
       <div>Score: {game.score}</div>
       <div>Health: {game.health}</div>
       <div>
