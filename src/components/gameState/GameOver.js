aa0a74e7d5686ff7ba14fc0582299b49f4e58cc8
Jacob Cho <jacobcho@live.com>
2021-12-07 16:15:42 +0000

    sounds

--- src/components/gameState/GameOver.js
+++ src/components/gameState/GameOver.js
@@ -1,9 +1,16 @@
 import { useScoreAPI } from "../../hooks/useScoreAPI";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
+import {Howl} from "howler";  // Howler JS Audio library
+import GameEndTheme from "../../audioclips/game_over.mp3" 
 
 export const GameOver = ({ gameScore }) => {
   useScoreAPI();
+
+  const gameover = new Howl({
+    src: GameEndTheme,
+  })
+  gameover.play() //game over mp3
 
   return (
     <div>
