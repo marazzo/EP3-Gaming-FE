dc685b430d5b50ddc3f8e4e2f81142cebdd3f150
Jacob Cho <jacobcho@live.com>
2021-12-08 11:26:39 +0000

    timer for monster made

--- src/components/gameState/GameView.js
+++ src/components/gameState/GameView.js
@@ -7,6 +7,7 @@ import { Grid } from "@material-ui/core";
 import {Howl} from "howler";  // Howler JS Audio library
 import Punch from "../../audioclips/hit.mp3" 
 import { ExitBox } from "./ExitBox";
+import React from 'react'
 
 export const GameView = () => {
   const [game, changeTurn] = useGameAPI(); //[gameData, changeTurn]
@@ -15,6 +16,7 @@ export const GameView = () => {
     src: Punch,
     volume: 0.8
   })
+   
 
   const handleClick = () => {
     changeTurn();
@@ -24,8 +26,24 @@ export const GameView = () => {
   const handleKeyPress = (event) => {
     if (event.code === "Space") {
       changeTurn();
+      punch.play()
     }
   };
+
+  const [seconds, setSeconds] = React.useState(5);
+
+  React.useEffect(() => {
+    if (seconds > 0) {
+      setTimeout(() => setSeconds(seconds - 1), 1000);
+    } else if (seconds == 0) {
+      {setSeconds('MONSTER ATTACKS YOU')}
+      setTimeout(() => {setSeconds(5)}, 1000)
+      return () => clearTimeout();
+    }
+  });
+ 
+
+
   return (
     <div className="game-container">
         <ExitBox />
@@ -43,13 +61,13 @@ export const GameView = () => {
           <Hero />
         </Grid>
         <Grid item xs={4}>
-          
+        <h1>{seconds}</h1>
         </Grid>
         <Grid item xs={4}>
           <Monster />
         </Grid>
         <Grid item xs={12}>
-          
+    
         </Grid>
       </Grid>
 
@@ -62,9 +80,11 @@ export const GameView = () => {
 
           </div>
         ) : (
+          <div>
           <button onClick={handleClick} onKeyPress={handleKeyPress}>
             Attack
           </button>
+          </div>
         )}
       </div>
     </div>
