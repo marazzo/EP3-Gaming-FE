7c894ac108955549b2f969db9c48726dbf2c4f32
Serena Bertozzi <serenabertozzi@protonmail.com>
2021-12-09 12:15:40 +0000

    Moar styles

--- src/components/gameState/GameOver.js
+++ src/components/gameState/GameOver.js
@@ -1,25 +1,26 @@
 import { useScoreAPI } from "../../hooks/useScoreAPI";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
-import {Howl} from "howler";  // Howler JS Audio library
-import React from 'react'
-import GameEndTheme from "../../audioclips/game_over.mp3" 
+import { Howl } from "howler"; // Howler JS Audio library
+import React from "react";
+import GameEndTheme from "../../audioclips/game_over.mp3";
+import { margin } from "@mui/system";
 
 export const GameOver = ({ gameScore }) => {
   useScoreAPI();
 
   const gameover = new Howl({
     src: GameEndTheme,
-    volume: 0.2
-  })
+    volume: 0.2,
+  });
 
-  gameover.play()
-  
+  gameover.play();
 
   return (
-    <div>
+    <div className="go-card">
       <h1>Game Over</h1>
-      <p>Your score is {gameScore}</p>
+      <p>Your score is:</p>
+      <p>{gameScore}</p>
       <Button
         component={Link}
         to="/scoreboard"
